12c77948a90dc7d6390ccef874b8db210505ffa8
aboveStars <yunuskorkmazis384@outlook.com>
2024-09-11 16:41:41 +0300

    minor changes.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { Layout } from "components/layout";
 
 import theme from "../theme";
 
-import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { announcement, header, footer } = pageProps;
@@ -15,7 +15,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <GoogleAnalytics
         gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY || ""}
       />
-      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_ID || ""} />
 
       <SaasProvider theme={theme}>
         <Layout
